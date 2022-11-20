458e62104981e71308884c6062d70d49314a4274
Adam Pluciński <adaskothebeast@gmail.com>
2022-11-20 12:41:44 +0100

    added duration handler

---
+++ test/unit/AdaskoTheBeAsT.Dapper.NodaTime.Test/DurationTests.cs
@@ -0,0 +1,56 @@
+using System.Linq;
+using Dapper;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using NodaTime;
+using Xunit;
+
+namespace AdaskoTheBeAsT.Dapper.NodaTime.Test
+{
+    [Collection("DBTests")]
+    public class DurationTests
+    {
+        private readonly string _connectionString;
+
+        public DurationTests(DatabaseFixture databaseFixture)
+        {
+            _connectionString = databaseFixture.ConnectionString;
+            SqlMapper.AddTypeHandler(DurationHandler.Default);
+        }
+
+        [Theory]
+        [ClassData(typeof(DbVendorLibraryTestData))]
+        public void Can_Write_And_Read_Duration_Stored_As_BigInt(DbVendorLibrary library)
+        {
+            using var connection = DbVendorLibraryConnectionProvider.Provide(library, _connectionString);
+            var o = new TestObject { Value = Duration.FromDays(365) };
+
+            const string sql = "CREATE TABLE #T ([Value] bigint); INSERT INTO #T VALUES (@Value); SELECT * FROM #T";
+            var t = connection.Query<TestObject>(sql, o).First();
+
+            t.Value.Should().Be(o.Value);
+        }
+
+        [Theory]
+        [ClassData(typeof(DbVendorLibraryTestData))]
+        public void Can_Write_And_Read_Duration_With_Null_Value_Stored_As_BigInt(DbVendorLibrary library)
+        {
+            using var connection = DbVendorLibraryConnectionProvider.Provide(library, _connectionString);
+            var o = new TestObject();
+
+            const string sql = "CREATE TABLE #T ([Value] bigint NULL); INSERT INTO #T VALUES (@Value); SELECT * FROM #T";
+            var t = connection.Query<TestObject>(sql, o).First();
+
+            using (new AssertionScope())
+            {
+                t.Value.Should().BeNull();
+                t.Value.Should().Be(o.Value);
+            }
+        }
+
+        private class TestObject
+        {
+            public Duration? Value { get; set; }
+        }
+    }
+}
