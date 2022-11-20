c09e567315a0f88ac0d425b1377ffa2330c927f5
Adam Pluciński <adaskothebeast@gmail.com>
2022-11-20 17:03:41 +0100

    period handler implementation

---
+++ src/AdaskoTheBeAsT.Dapper.NodaTime/PeriodHandler.cs
@@ -0,0 +1,178 @@
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using Dapper;
+using NodaTime;
+
+namespace AdaskoTheBeAsT.Dapper.NodaTime
+{
+    public sealed class PeriodHandler
+        : SqlMapper.TypeHandler<Period>
+    {
+        public const string Space = " ";
+        public static readonly PeriodHandler Default = new();
+#pragma warning disable MA0009 // Add regex evaluation timeout
+        private static readonly Regex PeriodRegex =
+            new Regex("(Y[:](?<years>[-]?\\d+))?\\s*(M[:](?<months>[-]?\\d+))?\\s*(W[:](?<weeks>[-]?\\d+))?\\s*(D[:](?<days>[-]?\\d+))?\\s*(h[:](?<hours>[-]?\\d+))?\\s*(m[:](?<minutes>[-]?\\d+))?\\s*(s[:](?<seconds>[-]?\\d+))?\\s*(ms[:](?<miliseconds>[-]?\\d+))?\\s*(t[:](?<ticks>[-]?\\d+))?\\s*(ns[:](?<nanoseconds>[-]?\\d+))?");
+#pragma warning restore MA0009 // Add regex evaluation timeout
+
+        private PeriodHandler()
+        {
+        }
+
+        public override void SetValue(IDbDataParameter parameter, Period value)
+        {
+            var list = new List<string>();
+            ProcessSetDateComponent(list, value);
+            ProcessSetTimeComponent(list, value);
+            ProcessSetSubSecondTimeComponent(list, value);
+
+            parameter.Value = string.Join(Space, list);
+            parameter.SetSqlDbType(SqlDbType.VarChar);
+        }
+
+        public override Period Parse(object value)
+        {
+            if (value is Period period)
+            {
+                return period;
+            }
+
+            if (value is string str)
+            {
+                var match = PeriodRegex.Match(str);
+                if (!match.Success)
+                {
+                    return Period.Zero;
+                }
+
+                var builder = new PeriodBuilder();
+                SetDateComponentInBuilder(builder, match);
+                SetTimeComponentInBuilder(builder, match);
+                SetSubSecondTimeComponentInBuilder(builder, match);
+                return builder.Build();
+            }
+
+            throw new DataException($"Cannot convert {value.GetType()} to NodaTime.Period");
+        }
+
+        private static void ProcessSetDateComponent(List<string> list, Period value)
+        {
+            if (value.Years != 0)
+            {
+                list.Add($"Y:{value.Years}");
+            }
+
+            if (value.Months != 0)
+            {
+                list.Add($"M:{value.Months}");
+            }
+
+            if (value.Weeks != 0)
+            {
+                list.Add($"W:{value.Weeks}");
+            }
+
+            if (value.Days != 0)
+            {
+                list.Add($"D:{value.Days}");
+            }
+        }
+
+        private static void ProcessSetTimeComponent(List<string> list, Period value)
+        {
+            if (value.Hours != 0)
+            {
+                list.Add($"h:{value.Hours}");
+            }
+
+            if (value.Minutes != 0)
+            {
+                list.Add($"m:{value.Minutes}");
+            }
+
+            if (value.Seconds != 0)
+            {
+                list.Add($"s:{value.Seconds}");
+            }
+        }
+
+        private static void ProcessSetSubSecondTimeComponent(List<string> list, Period value)
+        {
+            if (value.Milliseconds != 0)
+            {
+                list.Add($"ms:{value.Milliseconds}");
+            }
+
+            if (value.Ticks != 0)
+            {
+                list.Add($"t:{value.Ticks}");
+            }
+
+            if (value.Nanoseconds != 0)
+            {
+                list.Add($"ns:{value.Nanoseconds}");
+            }
+        }
+
+        private static void SetDateComponentInBuilder(PeriodBuilder builder, Match match)
+        {
+            if (match.Groups["years"].Success)
+            {
+                builder.Years = int.Parse(match.Groups["years"].Value, CultureInfo.InvariantCulture);
+            }
+
+            if (match.Groups["months"].Success)
+            {
+                builder.Months = int.Parse(match.Groups["months"].Value, CultureInfo.InvariantCulture);
+            }
+
+            if (match.Groups["weeks"].Success)
+            {
+                builder.Weeks = int.Parse(match.Groups["weeks"].Value, CultureInfo.InvariantCulture);
+            }
+
+            if (match.Groups["days"].Success)
+            {
+                builder.Days = int.Parse(match.Groups["days"].Value, CultureInfo.InvariantCulture);
+            }
+        }
+
+        private static void SetTimeComponentInBuilder(PeriodBuilder builder, Match match)
+        {
+            if (match.Groups["hours"].Success)
+            {
+                builder.Hours = long.Parse(match.Groups["hours"].Value, CultureInfo.InvariantCulture);
+            }
+
+            if (match.Groups["minutes"].Success)
+            {
+                builder.Minutes = long.Parse(match.Groups["minutes"].Value, CultureInfo.InvariantCulture);
+            }
+
+            if (match.Groups["seconds"].Success)
+            {
+                builder.Seconds = long.Parse(match.Groups["seconds"].Value, CultureInfo.InvariantCulture);
+            }
+        }
+
+        private static void SetSubSecondTimeComponentInBuilder(PeriodBuilder builder, Match match)
+        {
+            if (match.Groups["miliseconds"].Success)
+            {
+                builder.Milliseconds = long.Parse(match.Groups["miliseconds"].Value, CultureInfo.InvariantCulture);
+            }
+
+            if (match.Groups["ticks"].Success)
+            {
+                builder.Ticks = long.Parse(match.Groups["ticks"].Value, CultureInfo.InvariantCulture);
+            }
+
+            if (match.Groups["nanoseconds"].Success)
+            {
+                builder.Nanoseconds = long.Parse(match.Groups["nanoseconds"].Value, CultureInfo.InvariantCulture);
+            }
+        }
+    }
+}
---
+++ test/unit/AdaskoTheBeAsT.Dapper.NodaTime.Test/PeriodTests.cs
@@ -0,0 +1,97 @@
+using System.Linq;
+using Dapper;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using NodaTime;
+using Xunit;
+
+namespace AdaskoTheBeAsT.Dapper.NodaTime.Test
+{
+    [Collection("DBTests")]
+    public sealed class PeriodTests
+    {
+        private readonly string _connectionString;
+
+        public PeriodTests(DatabaseFixture databaseFixture)
+        {
+            _connectionString = databaseFixture.ConnectionString;
+            SqlMapper.AddTypeHandler(PeriodHandler.Default);
+        }
+
+        [Theory]
+        [ClassData(typeof(DbVendorLibraryTestData))]
+        public void Can_Write_And_Read_Period_Stored_As_Varchar(DbVendorLibrary library)
+        {
+            using var connection = DbVendorLibraryConnectionProvider.Provide(library, _connectionString);
+            var o = new TestObject
+            {
+                Value = new PeriodBuilder
+                {
+                    Years = int.MinValue,
+                    Months = int.MinValue,
+                    Weeks = int.MinValue,
+                    Days = int.MinValue,
+                    Hours = long.MinValue,
+                    Minutes = long.MinValue,
+                    Seconds = long.MinValue,
+                    Milliseconds = long.MinValue,
+                    Ticks = long.MinValue,
+                    Nanoseconds = long.MinValue,
+                }.Build(),
+            };
+
+            const string sql = "CREATE TABLE #T ([Value] varchar(195)); INSERT INTO #T VALUES (@Value); SELECT * FROM #T";
+            var t = connection.Query<TestObject>(sql, o).First();
+
+            t.Value.Should().Be(o.Value);
+        }
+
+        [Theory]
+        [ClassData(typeof(DbVendorLibraryTestData))]
+        public void Can_Write_And_Read_Period_With_Null_Value_Stored_As_Varchar(DbVendorLibrary library)
+        {
+            using var connection = DbVendorLibraryConnectionProvider.Provide(library, _connectionString);
+            var o = new TestObject();
+
+            const string sql = "CREATE TABLE #T ([Value] varchar(195) NULL); INSERT INTO #T VALUES (@Value); SELECT * FROM #T";
+            var t = connection.Query<TestObject>(sql, o).First();
+
+            using (new AssertionScope())
+            {
+                t.Value.Should().BeNull();
+                t.Value.Should().Be(o.Value);
+            }
+        }
+
+        [Fact]
+        public void PeriodHandler_Parse_Should_Return_Same_Period()
+        {
+            // Arrange
+            var handler = PeriodHandler.Default;
+            var period = new PeriodBuilder
+            {
+                Years = 1,
+                Months = 2,
+                Weeks = 3,
+                Days = 4,
+                Hours = 5,
+                Minutes = 6,
+                Seconds = 7,
+                Milliseconds = 8,
+                Ticks = 9,
+                Nanoseconds = 10,
+            }.Build();
+
+            // Act
+            var result = handler.Parse(period);
+
+            // Assert
+            result.Should().Be(period);
+        }
+
+        private class TestObject
+        {
+            public Period? Value { get; set; }
+        }
+    }
+}
